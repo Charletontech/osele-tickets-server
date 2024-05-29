0789bcf8dbb121d2989f36e2cbaa8028f811d8a6
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-05-28 23:01:02 -0700

    database details

--- server.js
+++ server.js
@@ -52,13 +52,12 @@ cloudinary.config({
 
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "osele",
+  host: `${process.env.HOST}`,
+  user: `${process.env.USER}`,
+  password: `${process.env.PASSWORD}`,
+  port: `${process.env.PORT}`,
+  database: `${process.env.DATABASE}`,
 });
-
 
 
 
